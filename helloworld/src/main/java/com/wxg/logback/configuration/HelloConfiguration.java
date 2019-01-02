72c2633080659e4a46cffe7886bc214e07be2b16
wxg <chixin0769@163.com>
2019-01-02 10:11:34 +0800

    logback

---
+++ helloworld/src/main/java/com/wxg/logback/configuration/HelloConfiguration.java
@@ -0,0 +1,11 @@
+package com.wxg.logback.configuration;
+
+/**
+ * 2019年1月2日00:09:35
+ * from : https://logback.qos.ch/manual/configuration.html
+ *
+ * {@link ch.qos.logback.classic.spi.Configurator}
+ * {@link ch.qos.logback.classic.BasicConfigurator}
+ */
+public class HelloConfiguration {
+}
---
+++ helloworld/src/main/java/com/wxg/logback/joran/HelloJoran.java
@@ -0,0 +1,18 @@
+package com.wxg.logback.joran;
+
+/**
+ * 2019年1月1日23:52:06
+ * from : https://logback.qos.ch/manual/onJoran.html
+ *
+ * {@link ch.qos.logback.core.joran.action.Action}
+ *
+ * {@link ch.qos.logback.core.joran.spi.RuleStore}
+ * {@link ch.qos.logback.core.joran.spi.SimpleRuleStore}
+ *
+ * {@link ch.qos.logback.core.joran.spi.InterpretationContext}
+ *
+ * {@link ch.qos.logback.core.status.StatusManager}
+ * {@link ch.qos.logback.core.BasicStatusManager}
+ */
+public class HelloJoran {
+}
