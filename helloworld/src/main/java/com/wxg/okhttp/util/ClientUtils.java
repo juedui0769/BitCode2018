6126ae664113ee3351239ede09db0214ec149af9
wxg <chixin0769@163.com>
2019-02-26 10:08:08 +0800

    添加 synchronized 关键字

--- helloworld/src/main/java/com/wxg/okhttp/util/ClientUtils.java
+++ helloworld/src/main/java/com/wxg/okhttp/util/ClientUtils.java
@@ -14,7 +14,11 @@ public class ClientUtils {
         return client;
     }
 
-    public static void destroy() {
+    /**
+     * 2019年2月26日10:04:17 update : 添加 `synchronized` 关键字
+     * 但是, `client`有关键字`volatile`修饰, 如果不加 `synchronized` 也不会被重排序吧？
+     */
+    public synchronized static void destroy() {
         if (client != null) {
             client.dispatcher().executorService().shutdown();   //清除并关闭线程池
             client.connectionPool().evictAll();                 //清除并关闭连接池
