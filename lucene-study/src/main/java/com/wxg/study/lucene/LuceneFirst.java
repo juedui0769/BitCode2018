766c67a49be0a8fb6950451abc513f740b0c903f
juedui0769 <chixin0769@163.com>
2019-05-27 21:55:47 +0800

    lucene

--- lucene-study/src/main/java/com/wxg/study/lucene/LuceneFirst.java
+++ lucene-study/src/main/java/com/wxg/study/lucene/LuceneFirst.java
@@ -1,6 +1,10 @@
 package com.wxg.study.lucene;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.TextField;
@@ -11,6 +15,7 @@ import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.RAMDirectory;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.wltea.analyzer.lucene.IKAnalyzer;
 
 import java.io.File;
 import java.io.IOException;
@@ -38,8 +43,12 @@ public class LuceneFirst {
     }
 
     /**
+     * 此方法是构建索引, 使用一次即可。 已添加`@Ignore`标记
+     *
      * {@link RAMDirectory} 内存中
      * {@link org.apache.lucene.store.FSDirectory} 磁盘上
+     *
+     * {@link StandardAnalyzer} 标准分析器
      *
      * @throws Exception
      */
@@ -80,6 +89,11 @@ public class LuceneFirst {
         }
     }
 
+    /**
+     * 查询
+     *
+     * @throws Exception
+     */
     @Test
     public void searchIndex() throws Exception {
         IndexReader indexReader = DirectoryReader.open(indexStoreDir);
@@ -100,6 +114,57 @@ public class LuceneFirst {
             System.out.println("==================");
         }
         indexReader.close();
+    }
+
+    /**
+     * 演示: 查看标准分词器的分词效果
+     * 英文, 标准分词器，表现良好；
+     * 中文，就只能拆分成一个一个的单词。
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testTokenStream() throws IOException {
+        Analyzer analyzer = new StandardAnalyzer();
+        String text = "Jack opened his eyes. He was still pointing at the picture of the Frog Creek woods.";
+        text = "2018年9月27日 -  Elasticsearch:基于Lucene开发的企业级的搜索引擎产品 5 Lucene的基本使用 使用Lucene的API来实现对索引的增(创建索引)、删(删除索引)、改(修改索引).";
+        TokenStream tokenStream = analyzer.tokenStream("", text);
+
+        CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
+        tokenStream.reset();
+
+        while (tokenStream.incrementToken()) {
+            System.out.println(charTermAttribute.toString());
+        }
+
+        tokenStream.close();
+    }
+
+    /**
+     * https://www.oschina.net/p/ikanalyzer
+     * 使用失败, 我是通过maven加入依赖的，但是，可能因为不兼容，导致出错。
+     * https://github.com/yozhao/IKAnalyzer
+     *
+     * 其他的中文分词 : https://www.oschina.net/p/paoding [庖丁]
+     * https://www.oschina.net/p/ansj [ansj]
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testIK() throws IOException {
+        Analyzer analyzer = new IKAnalyzer();
+        String text = "Jack opened his eyes. He was still pointing at the picture of the Frog Creek woods.";
+        text = "2018年9月27日 -  Elasticsearch:基于Lucene开发的企业级的搜索引擎产品 5 Lucene的基本使用 使用Lucene的API来实现对索引的增(创建索引)、删(删除索引)、改(修改索引).";
+        TokenStream tokenStream = analyzer.tokenStream("", text);
+
+        CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
+        tokenStream.reset();
+
+        while (tokenStream.incrementToken()) {
+            System.out.println(charTermAttribute.toString());
+        }
+
+        tokenStream.close();
     }
 
 }
