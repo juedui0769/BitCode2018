a054962cfc5b35da7b7fb6fa915d15c0bddf951c
juedui0769 <chixin0769@163.com>
2019-08-26 08:35:09 +0800

    JavaNIO

---
+++ JavaNIO/README.md
@@ -0,0 +1,10 @@
+
+### Java NIO 系列教程
+
+总览： <http://ifeve.com/java-nio-all/>
+
+001 概述： <http://ifeve.com/overview/>
+
+
+
+
---
+++ JavaNIO/src/test/java/JavaNIOTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JavaNIOTest {
+
+    final Logger logger = LoggerFactory.getLogger(JavaNIOTest.class);
+
+    @Test
+    public void test001() {
+        logger.info("first , init , 输出日志");
+    }
+}
